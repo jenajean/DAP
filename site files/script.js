79f5186cd3af4f0729b413f7d23c18570997a759
Andrew Brentano <andrew@artichokejalapenodesign.com>
2013-01-21 15:28:26 -0800

    not-yet-reliable function for pulling sub-navigation's content

--- site files/script.js
+++ site files/script.js
@@ -2,32 +2,53 @@ $(function() {
 
 
 
-//switch out all hrefs for "#"
-$('.main-nav a').each(function(){
-	$cur_link = $(this);
-	var href = $cur_link.attr('href');
-	console.log("href: " + href);//test code
-	$cur_link.attr('rel', href);
-	console.log("rel: " + $cur_link.attr('rel'));//test code
-	$cur_link.attr('href', '#');
-	console.log("final href: " + $cur_link.attr('href'));//test code
-	console.log("Links are sanitized");
-});
-
+	//function to switch out all hrefs in the specified container with "#"
+	function sanitizeLinks(container){
+	$container = $(container);
+	console.log($container.html());
+	//first check to make sure the container hasn't already been sanitized so the rel values aren't also overwritten with "#"
+		if ($container.hasClass("sanitized")){
+			return;//do nothing
+		}
+		else{
+			$(container + ' a').each(function(){
+				$cur_link = $(this);
+				var href = $cur_link.attr('href');
+				//console.log("href: " + href);//test code
+				$cur_link.attr('rel', href);
+				//console.log("rel: " + $cur_link.attr('rel'));//test code
+				$cur_link.attr('href', '#');
+				//console.log("final href: " + $cur_link.attr('href'));//test code
+				$container.addClass("sanitized");
+			});
+			
+		console.log("Links are sanitized");	
+		console.log($container.html());
+		return;
+		}
+	}
 
 
 //VVV----- Code for dynamic page content replacement -----VVV (thanks to Jesse Shawl and CSS Tricks for the example code)
-	if(Modernizr.history){
 	
-		//run link sanitizer to prevent actual navigation off the page
+	var hasHistory = false;
+	if(Modernizr.history){
+		hasHistory = true;
+	}
+	
+	if(hasHistory){
+	
+		
+		
+		//run link sanitizer to prevent actual navigation off the page, only if we have access to history
+		sanitizeLinks('.main-nav');
+		sanitizeLinks('.sub-nav');
 		
 		
 	    //setup variables for dynamic content replacement
-	    var newHash      = "",
-	        $dynamicContentWrap = $("#dynamic_content_wrap"),
+	    var $dynamicContentWrap = $("#dynamic-content-wrap"),
 	        $pageWrap    = $("body"), 
-	        baseHeight   = 0,
-	        $el;
+	        baseHeight   = 0;
 	        
 	        var $subNav = $("#sub-nav-wrap");//for sub-nav replacement
 	        
@@ -39,18 +60,33 @@ $('.main-nav a').each(function(){
 	    
 	    
 	    
-	    //when a main navigation link is clicked, run the loadContent function
+	    //when a main navigation link is clicked, give it the 'selected' class and run the loadContent and loadSubNav functions
 	    $('.main-nav a').click(function() {
+	        $('.main-nav a').removeClass("selected");//un-select the currently selected link
+	        $(this).addClass("selected"); //add selected class to the current
 	        var _link = $(this).attr("rel");
 	        var _link2 = _link;
-	        console.log("LINK: "+_link);
-	        console.log("LINK2: "+_link2);
+	        //console.log("LINK: "+_link);//test code
+	        //console.log("LINK2: "+_link2);//test code
 	        loadContent(_link);
 	        loadSubNav(_link2);
 	        history.pushState(null, null, _link);
 	        return false;
 	    });
 	    
+	    //when a sub navigation link is clicked, give it the 'selected' class and run the loadContent function
+	    $('.sub-nav a').click(function() {
+	        $('.sub-nav a').removeClass("selected");//un-select the currently selected link
+	        $(this).addClass("selected"); //add selected class to the current
+	        var _link = $(this).attr("rel");
+	        console.log("subLINK: "+_link);//test code
+	        loadContent(_link);
+	        history.pushState(null, null, _link);
+	        return false;
+	    });
+	    
+
+			    
 	    
   
 	    //function to dynamically replace page content
@@ -65,33 +101,26 @@ $('.main-nav a').each(function(){
 	                            $pageWrap.animate({
 	                                height: baseHeight + $dynamicContentWrap.height() + "px"
 	                            });//end animate
-	                            
 	                        });//end fadeIn
-	                        
-//**temporarily disable class re-assignment until final classes are decided**
-//							$("nav a").removeClass("current");
-// 	                        console.log(href);
-// 	                        $("nav a[href$="+href+"]").addClass("current");
-
 	                    });//end load
-	                    
 	                });//end fadeOut
-	                
 	    }//end loadContent definition
+	    
+	    
 	    
 	    //function to dynamically replace sub-nav content
 	    function loadSubNav(href){
-	    	console.log("argument: "+ href);//test code
-	    	
+	    	console.log("argument subnav: "+ href);//test code
+	    	//replace sub-nav
 	    	$subNav.find(".sub-nav").fadeOut(100, function(){
 	    		$subNav.hide().load(href + " .sub-nav", function(){
 	    			$subNav.fadeIn(100, function(){
-	    				//nothing special here for the time being
+	    				sanitizeLinks('.sub-nav'); //sanitize the links in the new subnav
+	    				//nothing special animated here for the time being
 	    			});
 	    		});//end load f(x)
-	    	});//end fadeOut f(x)
-	    	
-	    	}//end loadSubNav definition
+	    	});//end fadeOut f(x)	
+	    }//end loadSubNav definition
 	    
 	    
 	    
@@ -104,8 +133,7 @@ $('.main-nav a').each(function(){
 	
 	
 	
-	} // otherwise, history is not supported, so nothing fancy here.
-	//end of if(Modernizr.history..)
+	} // otherwise, history is not supported, so nothing fancy here. - end of if(Modernizr.history..)
 
 //^^^----- Code for dynamic page content replacement -----^^^
 
